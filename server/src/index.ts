a0a48fc81e96260144ddc18df88513d223c2107b
LSY <lsy@LSYs-MacBook-Air.local>
2023-07-31 18:54:27 -0400

    v5

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import { quizRouter  } from './router/router';
 import cors from 'cors';
+import path from 'path';
+
 const app = express();
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(cors());
-
-
 app.use(quizRouter);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+  });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
